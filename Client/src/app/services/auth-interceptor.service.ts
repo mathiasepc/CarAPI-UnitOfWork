7d6fa86731da3f86b02b3f1ee3d87215b85af0b7
mathias clausen <70236153+mathiasepc@users.noreply.github.com>
2024-08-08 15:23:23 +0200

    Auth0 virker. Mangler admin, user osv.

--- Client/src/app/services/auth-interceptor.service.ts
+++ Client/src/app/services/auth-interceptor.service.ts
@@ -16,20 +16,30 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private auth: AuthService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    return from(this.auth.getAccessTokenSilently().pipe(
-      switchMap(idToken => {
-
-        console.log("idToken: ", idToken);
-        const authReq = req.clone({
-          setHeaders: {
-            Authorization: `Bearer ${idToken}`
-          }
-        });
-
-        return next.handle(authReq)
-      })
-    ))
+    // 'from' konverterer en Observable eller Promise til en ny Observable
+    return from(
+      // Hent access token JWT (silent authentication)
+      this.auth.getAccessTokenSilently().pipe(
+        // switchMap bruges til at skifte fra den første Observable (token-forespørgsel)
+        // til en ny Observable (HTTP-anmodningen med token)
+        switchMap(idToken => {
+          // Log tokenen til debugging
+          // console.log("idToken: ", idToken);
+  
+          // Klon den oprindelige HTTP-anmodning og tilføj Authorization-headeren med Bearer token
+          const authReq = req.clone({
+            setHeaders: {
+              // Tilføjer token som en Bearer token i Authorization-headeren
+              Authorization: `Bearer ${idToken}` 
+            }
+          });
+  
+          // Send den modificerede HTTP-anmodning videre til næste handler i rækken.
+          return next.handle(authReq)
+        })
+      )
+    );
   }
   
-
 }
+  
